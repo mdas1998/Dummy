b21691bc02e5452450c82ef6b0826db95a046071
mdas1998 <michealdas1998@gmail.com>
2023-11-20 17:19:18 +0530

    Added 3 new ML models

--- ml_mode.py
+++ ml_mode.py
@@ -1,1 +1,11 @@
 print("ML model for fraud detection")
+
+
+def model1():
+    print("This is ML model 1")
+
+def model2():
+    print("This is ML model 2")
+
+def model3():
+    print("This is ML model 3 ")
